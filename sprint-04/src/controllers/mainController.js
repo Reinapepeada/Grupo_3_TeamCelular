2f8311f4408bb42393126e941a85171910038a1a
Reinapepeada <abiarrieta@uade.edu.ar>
2022-09-11 16:37:07 -0300

    avanzando con el CRUD de usuario

--- sprint-04/src/controllers/mainController.js
+++ sprint-04/src/controllers/mainController.js
@@ -1,7 +1,22 @@
 
 let products = require('../../public/js/products')
-
+const fs = require('fs');
+const multer = require('multer');
+const { json } = require('body-parser');
 const path = require('path');
+
+let usersFilePath = path.join(__dirname, '../../data/users.json');
+let users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+
+
+
+const { validationResult } = require('express-validator');
+
+
+//abro el products.json y lo convierto a javascript con parse
+const jsonData = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'))
+
+
 
 let mainController = {
     products: products,
@@ -27,5 +42,23 @@ let mainController = {
         res.render('services')
         next();
     },
+
+    createUser:(req,res)=>{
+        if(jsonData == ''){
+            jsonData =[];
+          }
+          let user={}
+          user={
+            id:Math.random(),
+            nickname:req.body.nickname,
+            email:req.body.email,
+            password:req.body.password
+          }
+          console.log(user)
+          jsonData.push(user)
+          jsonData_two = JSON.stringify(jsonData); //a json
+          fs.writeFileSync('./data/users.json', jsonData_two);
+          res.redirect("/")
+    }
 }
 module.exports = mainController;
--- sprint-04/src/routes/main.js
+++ sprint-04/src/routes/main.js
@@ -4,6 +4,7 @@ const mainController = require("../controllers/mainController")
 
 router.get('/', mainController.list )
 router.get('/login', mainController.login )
+router.post('/register', mainController.createUser )
 router.get('/register', mainController.register )
 router.get('/services', mainController.services )
 
